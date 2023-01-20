bb2007f6f1d3ee6d94a9fe458480fad29f190644
newtoncy <1213148423@qq.com>
2023-01-20 11:32:32 +0800

    Readme and usability

--- dialog.py
+++ dialog.py
@@ -19,6 +19,20 @@ class SetHourTodayDialog(QDialog):
         self.ui.buttonBox.accepted.connect(self.accepted)
         self.ui.buttonBox.rejected.connect(lambda: self.close())
 
+    def exec(self):
+        from mytime import MyDateTime
+        t = MyDateTime.now()
+        pre_day = MyDateTime(t.stage, t.cycle, t.day - 1)
+        today = MyDateTime(t.stage, t.cycle, t.day)
+        next_day = MyDateTime(t.stage, t.cycle, t.day + 1)
+        if (t - today).total_seconds() > 4 * 3600:
+            hours = (next_day - today).total_seconds() / 3600
+        else:
+            hours = (today - pre_day).total_seconds() / 3600
+        self.ui.doubleSpinBox.setValue(hours)
+        super().exec()
+
+
     def accepted(self):
         command.set_today_hours(self.ui.doubleSpinBox.value())
         self.close()
